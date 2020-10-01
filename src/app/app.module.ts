73a747fde60baf8b929486dffa098c1f7442c4c7
Andrea Calabretta <andreacalabretta0@gmail.com>
2020-10-01 10:37:45 +0200

    23. Diving Into Structural Directives (1 pt.)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,21 @@ import { FormsModule } from '@angular/forms';
 import { MatInputModule,
         MatCardModule,
         MatButtonModule,
-        MatToolbarModule } from '@angular/material';
+        MatToolbarModule,
+        MatExpansionModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
     HeaderComponent,
+    PostListComponent,
+
   ],
   imports: [
     BrowserModule,
@@ -24,7 +28,8 @@ import { HeaderComponent } from './header/header.component';
     MatInputModule,
     MatCardModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
