b7e3a71b4bc86d751c70ad62054cc9b6ea3aa1f5
Andrea Calabretta <andreacalabretta0@gmail.com>
2020-10-02 10:23:56 +0200

    26. Added Forms

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Post } from '../post.model';
 
 @Component({
@@ -16,10 +17,13 @@ export class PostCreateComponent implements OnInit {
   enteredContent = '';
   @Output() postCreated = new EventEmitter <Post>();
 
-  onAddPost(){
+  onAddPost(form: NgForm){
+    if (form.invalid) {
+      return;
+    }
     const post: Post = {
-      title: this.enteredTitle,
-      content: this.enteredContent
+      title: form.value.title,
+      content: form.value.content
     };
     this.postCreated.emit(post);
   }
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Post} from '../post.model'
 
 @Component({
   selector: 'app-post-list',
@@ -16,6 +17,6 @@ export class PostListComponent implements OnInit {
   //   {title: "Second Post", content: "This is the second post\'s content"},
   //   {title: "First Post", content: "This is the third post\'s content"}
   // ];
- @Input() posts = []; //No posts added yet
+ @Input() posts: Post[] = []; //No posts added yet
 
 }
