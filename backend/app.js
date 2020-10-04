49a6a49ae5127e1cff5a44197b4c83c101b913e5
Andrea Calabretta <andreacalabretta0@gmail.com>
2020-10-04 17:41:41 +0200

    38. Fetching Initial Posts

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First Middleware!');
-  console.log(req.method, req.path) //vediamo perchè il middleware esegue 2 volte: c'è anche la richiesta del favicon, ecco perchè
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('Hello from express!')
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: "ijsdaofjosad2334",
+      title: "First server-side post",
+      content: "This is coming from the server"
+    },
+    {
+      id: "ijsda3453osad2334",
+      title: "Second server-side post",
+      content: "This is coming from the server!"
+    }
+  ];
+  res.status(200).json({
+    message: "Posts fetched successfully",
+    posts: posts
+  });
 });
 
 module.exports = app; //in questo modo esportiamo anche tutti i middleware
