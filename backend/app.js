3da8cfbd083f3c18f44d3d12fab95d2c273dc83f
Andrea Calabretta <andreacalabretta0@gmail.com>
2020-10-11 14:12:50 +0200

    42. Adding Angular

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended : false }));
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*"); //non importa il dominio della richiesta, ma gli è permesso di accedere alle risorse
   res.setHeader(
-  "Access-Control-Allow-Header",
+  "Access-Control-Allow-Headers",
   "Origin, X-Requested-With, Content-Type, Accept"
   ); //la richiesta può avere anche questi extra Header e in questo caso gli è sempre permesso accedere alle risorse, se ha degli header non definiti qui, la richiesta verrà bloccata
   res.setHeader(
@@ -19,9 +19,9 @@ app.use((req, res, next) => {
   next();
 });
 
-api.post("/api/posts", (req, res, next) => {
+app.post("/api/posts", (req, res, next) => {
   const posts = req.body;
-  console.log(post);
+  console.log(posts);
   res.status(201).json( {
     message: 'Post added successfully'
   });
--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -30,6 +30,11 @@ export class PostService{
       title: title,
       content: content
     };
+    this.http.post<{ message: string }>("http://localhost:3000/api/posts", post)
+    .subscribe((responseData) => {
+      console.log(responseData.message);
+    });
+
     this.posts.push(post);
     this.postsUpdated.next([...this.posts]);
 
