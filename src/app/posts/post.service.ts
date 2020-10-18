a73601c71373e7dab6354b6b78980a0f3acff517
Andrea Calabretta <andreacalabretta0@gmail.com>
2020-10-18 12:15:55 +0200

    56. Trasforming Response Data

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import {Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 import { Post } from './post.model';
 
 @Injectable({providedIn: 'root'}) //serve a creare una sola instanza dei post per l'intera app
@@ -14,9 +16,20 @@ export class PostService{
 
 
   getPosts(){
-    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
-    .subscribe((postData) => {
-      this.posts = postData.posts;
+    this.http.get<{message: string, posts: any}>(
+      'http://localhost:3000/api/posts'
+      )
+      .pipe(map( (postData) => {
+        return postData.posts.map( post => {
+          return {
+            title: post.title,
+            content: post.content,
+            id: post._id
+          }
+        });
+      }))
+    .subscribe((transformedPosts) => {
+      this.posts = transformedPosts;
       this.postsUpdated.next([...this.posts]);
     });
   }
